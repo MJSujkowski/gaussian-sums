5d6b52045073ad2eeb5b4cb1046b2de493ebed4c
Mike Sujkowski <johnquerque@gmail.com>
2019-01-22 16:40:51 -0700

    Add while and scanner.

--- src/edu/cnm/deepdive/Gauss.java
+++ src/edu/cnm/deepdive/Gauss.java
@@ -1,22 +1,29 @@
 package edu.cnm.deepdive;
 
+import java.util.Scanner;
+
 public class Gauss {
-}
+
   public static final int DEFAULT_UPPER_LIMIT = 100;
+
   public static void main(String[] args) {
     // TODO READ upper limit from ags.
     int upperLimit = DEFAULT_UPPER_LIMIT;
-    long sum = triangleSum(upperLimit);
-    System.out.printf("Sum from 1 to %,d = %d%n", upperLimit, sum);
-  }
-
-
-  public static long triangleSum(int upperLimit) {
-    long sum = 0;
-    for (int i = 1; i <= upperLimit; i++) {
-      sum += i;
+      Scanner scanner = new Scanner(System.in);// Create instance of Scanner class, using System
+      System.out.print("Upper limit: ");
+      while (scanner.hasNextInt()) {
+        upperLimit = scanner.nextInt();
+        long sum = triangleSum(upperLimit);
+        System.out.printf("Sum from 1 to %, d = % d%n", upperLimit, sum);
+        System.out.print("Upper limit: ");
+      }
     }
-    return sum;
-  }
+    public static long triangleSum(int upperLimit){
+      long sum = 0;
+      for (int i = 1; i <= upperLimit; i++) {
+        sum += i;
+      }
+      return sum;
+    }
 
-}
+  }
